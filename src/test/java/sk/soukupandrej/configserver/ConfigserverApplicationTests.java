653cb850f7aea16c790797a7a9e9463b08ccc6d5
soukupa <soukup.andrej@gmail.com>
2023-03-07 21:51:33 +0100

    initial commit

---
+++ src/test/java/sk/soukupandrej/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.soukupandrej.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
